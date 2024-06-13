e446b771bd78cb83210e447af1dff7c9c7c16254
savedinstancestate <dayoung2335@naver.com>
2024-06-13 18:11:29 +0900

    [docs] route 추가

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/HomePage/Home';
 import Login from './pages/LoginPage/Login';
+import Redirection from "./pages/LoginPage/Redirection";
 import MyStudy from "./pages/MyStudyPage/MyStudy";
 import MyPage from './pages/MyPage/MyPage';
-
 
 const App = () => {
     return (
@@ -12,6 +12,7 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/redirection" element={<Redirection />} />
                 <Route path="/mystudy" element={<MyStudy />} />
                 <Route path="/mypage" element={<MyPage />} />
             </Routes>
