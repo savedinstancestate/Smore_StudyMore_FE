11834ca1f61f6ddb9c5acbab98abe8b502c2e6c2
savedinstancestate <dayoung2335@naver.com>
2024-06-14 18:12:41 +0900

    [style] 함수 선언방식 변경

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -5,18 +5,18 @@ import Login from '../LoginPage/Login';
 const App = () => {
   const [isModalOpen, setIsModalOpen] = useState(false); 
 
-  const handleOpenModal = () => {
+  function handleOpenModal() {
     setIsModalOpen(true); 
   };
 
-  const handleCloseModal = () => {
+  function handleCloseModal() {
     setIsModalOpen(false); 
   };
 
   return (
     <div>
       <button onClick={handleOpenModal}>로그인 모달 열기</button>
-      <Modal show={isModalOpen} onHide={handleCloseModal} title="로그인">
+      <Modal show={isModalOpen} handleClose={handleCloseModal} title="로그인">
         <Login />
       </Modal>
     </div>
