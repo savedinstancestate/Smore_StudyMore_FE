f9d532b45b6fb2ee7ead3dfaba39635b67d38d0b
savedinstancestate <dayoung2335@naver.com>
2024-06-17 16:00:07 +0900

    [docs] 문서 수정

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,19 +1,26 @@
 import { rest } from 'msw';
 
 export const handlers = [
-  rest.get('', (req, res, ctx) => {
+  rest.get('/study', (req, res, ctx) => {
     console.log('');
     return res(
       ctx.status(200),
       ctx.json({
         "studyList": [
-            
+          {
+            "studyPk": 1,
+            "studyName": "스터디 이름",
+            "studyImg": "/img/study-img-sample.png",
+            "studyPersonNum": 4,
+            "studyStartDate": "2023-01-01",
+            "studyEndDate": "2023-12-31"
+        },
             // 더 많은 스터디 데이터..
         ]
       })
     );
   }),
-  rest.get('', (req, res, ctx) => {
+  rest.get('/study/admin', (req, res, ctx) => {
     console.log('');
     return res(
       ctx.status(200),
@@ -22,7 +29,7 @@ export const handlers = [
             {
                 "studyPk": 1,
                 "studyName": "스터디 이름",
-                "studyImg": "img/study-img-sample.png",
+                "studyImg": "/img/study-img-sample.png",
                 "studyPersonNum": 4,
                 "studyStartDate": "2023-01-01",
                 "studyEndDate": "2023-12-31"
@@ -30,7 +37,7 @@ export const handlers = [
             {
               "studyPk": 2,
               "studyName": "스터디 이름2",
-              "studyImg": "img/study-img-sample.png",
+              "studyImg": "/img/study-img-sample.png",
               "studyPersonNum": 4,
               "studyStartDate": "2023-01-01",
               "studyEndDate": "2023-12-31"
@@ -38,7 +45,7 @@ export const handlers = [
           {
             "studyPk": 3,
             "studyName": "스터디 이름3",
-            "studyImg": "img/study-img-sample.png",
+            "studyImg": "/img/study-img-sample.png",
             "studyPersonNum": 4,
             "studyStartDate": "2023-01-01",
             "studyEndDate": "2023-12-31"
@@ -48,21 +55,61 @@ export const handlers = [
     );
   }),
 
-  rest.get('', (req, res, ctx) => {
-    const { studyPk } = req.params;  // 요청 파라미터에서 studyPk 추출
-  console.log(`studyPk: ${studyPk}에 대한 지원 목록을 가져옵니다.`);
+  rest.get('/study/apply/:studyPk', (req, res, ctx) => {
+    const { studyPk } = req.params;
+    console.log(`studyPk: ${studyPk}에 대한 지원 목록을 가져옵니다.`);
+    
+    // studyPk별로 다른 데이터를 반환하는 조건문 추가
+    let enterStudyList;
+    if (studyPk === "1") {
+      enterStudyList = [
+        {
+          "userId": 3333,
+          "nickname": "제로님이시다",
+          "profileURL": "/img/default-profile.png",
+          "content": "도전장이다...",
+        },
+        {
+          "userId": 3334,
+          "nickname": "바질아범",
+          "profileURL": "/img/default-profile.png",
+          "content": "지원합니다!",
+        },
+        {
+          "userId": 3339,
+          "nickname": "할수있수연",
+          "profileURL": "/img/default-profile.png",
+          "content": "나를받아줘 나를받아줘 나를받아줘 나를받아줘 나를받아줘 나를받아줘 나를받아줘 나를받아줘 나를받아줘 나를받아줘 나를받아줘 나를받아줘 나를받아줘 나를받아줘 나를받아줘 나를받아줘 나를받아줘 나를받아줘 나를받아줘 나를받아줘 나를받아줘 나를받아줘 나를받아줘 나를받아줘 나를받아줘 나를받아줘 나를받아줘 나를받아줘 ",
+        }
+      ];
+    } else if (studyPk === "2") {
+      enterStudyList = [
+        {
+          "userId": 3335,
+          "nickname": "zxcv",
+          "profileURL": "/img/default-profile.png",
+          "content": "저도 참여하고 싶어요!",
+        }
+      ];
+    } else {
+      enterStudyList = [];  // studyPk가 다른 경우 빈 배열 반환
+    }
+
     return res(
       ctx.status(200),
       ctx.json({
-        "enterStudyList": [
-            {
-                "userId" : 3333,
-                "nickname": "asdf",
-                "profileURL": "img/study-img-sample.png",
-                "content": "도전장이다...",
-            },
-        ]
+        "enterStudyList": enterStudyList
       })
     );
-  })
+  }),
+
+  rest.get('/users/mypage', (req, res, ctx) => {
+    console.log('');
+    return res(
+      ctx.status(200),
+      ctx.json({
+            "nickname": "제로다냥",
+      })
+    );
+  }),
 ];
