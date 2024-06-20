0b104f397c2830c2be5dc8442496c69b53ffa89e
ssssuji <00.prft@gmail.com>
2024-06-20 15:30:20 +0900

    [feat] 개인목표 리스트, 진행상황 별 리스트

---
+++ src/pages/StudyPage/PersonalGoals/NotStarted.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Goals.css';
+
+const NotStarted = () => {
+    const goals = [
+        { id: 1, avatar: '/path/to/avatar1.png', name: '이수지', content: '이번주까지 끝낼 수 있을까? 진짜?' },
+        { id: 2, avatar: '/path/to/avatar1.png', name: '이수지', content: '이번주까지 끝낼 수 있을까? 진짜?' },
+        { id: 3, avatar: '/path/to/avatar1.png', name: '이수지', content: '이번주까지 끝낼 수 있을까? 진짜?' },
+        { id: 4, avatar: '/path/to/avatar1.png', name: '이수지', content: '이번주까지 끝낼 수 있을까? 진짜?' },
+        { id: 5, avatar: '/path/to/avatar1.png', name: '이수지', content: '이번주까지 끝낼 수 있을까? 진짜?' },
+        { id: 6, avatar: '/path/to/avatar1.png', name: '이수지', content: '이번주까지 끝낼 수 있을까? 진짜?' },
+        { id: 6, avatar: '/path/to/avatar1.png', name: '이수지', content: '이번주까지 끝낼 수 있을까? 진짜?' },
+        { id: 6, avatar: '/path/to/avatar1.png', name: '이수지', content: '이번주까지 끝낼 수 있을까? 진짜?' },
+
+        { id: 6, avatar: '/path/to/avatar1.png', name: '이수지', content: '이번주까지 끝낼 수 있을까? 진짜?' },
+    ];
+
+    return (
+        <div className="personal-goals">
+            <div className="goal-header">진행 전 🛫</div>
+            <div className="goal-container">
+                {goals.map((goal) => (
+                    <div key={goal.id} className="goal">
+                        <img src={goal.avatar} alt={`${goal.name}의 사진`} className="goal-avatar" />
+                        <div className="goal-name">{goal.name}</div>
+                        <div className="goal-content">{goal.content}</div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default NotStarted;
