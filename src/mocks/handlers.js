9d55b9a5527ea40bfd63c33e97bc3c812f433178
savedinstancestate <dayoung2335@naver.com>
2024-06-28 10:26:32 +0900

    [docs] 테스트 데이터 수정

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,4 +1,4 @@
-import { rest } from 'msw';
+import { setupWorker, rest } from 'msw';
 
 export const handlers = [
   rest.get('/mystudy', (req, res, ctx) => {
@@ -15,7 +15,14 @@ export const handlers = [
             "studyStartDate": "2023-01-01",
             "studyEndDate": "2023-12-31"
         },
-            // 더 많은 스터디 데이터..
+        {
+          "studyPk": 1,
+          "studyName": "네크워King",
+          "studyImg": "/img/sample-img-2.png",
+          "studyPersonNum": 4,
+          "studyStartDate": "2023-01-01",
+          "studyEndDate": "2023-12-31"
+      },
         ]
       })
     );
@@ -112,7 +119,6 @@ export const handlers = [
   }),
 
   rest.get('/users/mypage', (req, res, ctx) => {
-    console.log('');
     return res(
       ctx.status(200),
       ctx.json({
@@ -121,12 +127,10 @@ export const handlers = [
     );
   }),
 
-  rest.get('/board', (req, res, ctx) => { // 이름, 내용, 사진, 일정, 인원
-    console.log('');
+  rest.get('/board', (req, res, ctx) => {
     return res(
-      ctx.status(200),
-      ctx.json({
-        "boardList": [
+        ctx.status(200),
+        ctx.json([
             {
                 "studyBoardPk": 1,
                 "studyPk": 1,
@@ -137,91 +141,86 @@ export const handlers = [
                 "studyPersonNum": 4,
                 "studyStartDate": "2023-01-01",
                 "studyEndDate": "2023-12-31",
-                "modifyDate": "2023-02-23",
+                "modifyDate": "2023-02-23"
             },
             {
-              "studyBoardPk": 2,
-              "studyPk": 2,
-              "studyName": "아마zone",
-              "adTitle": "아마존에 간 건 아마...존?",
-              "adContent": "아마존에 간 존에 대해 이야기하는 스터디입니다... 나이, 성별 제한 없습니다... 아무나 오세요... 환영합니다... 칼수락 합니다...",
-              "studyImg": "/img/sample-img-2.png",
-              "studyPersonNum": 4,
-              "studyStartDate": "2023-01-01",
-              "studyEndDate": "2023-12-31",
-              "modifyDate": "2023-02-23",
-          },
-          {
-            "studyBoardPk": 3,
-            "studyPk": 3,
-            "studyName": "우아한 스터디",
-            "adTitle": "우아한 스터디 모집합니다.",
-            "adContent": "우아한 스터디 모집중입니다! 우아한 스터디 모집중입니다! 우아한 스터디 모집중입니다! 우아한 스터디 모집중입니다! 우아한 스터디 모집중입니다! 우아한 스터디 모집중입니다!",
-            "studyImg": "/img/sample-img-3.jpg",
-            "studyPersonNum": 4,
-            "studyStartDate": "2023-01-01",
-            "studyEndDate": "2023-12-31",
-            "modifyDate": "2023-02-23",
-        },
-        
-        ]
-      })
+                "studyBoardPk": 2,
+                "studyPk": 2,
+                "studyName": "아마zone",
+                "adTitle": "아마존에 간 건 아마...존?",
+                "adContent": "아마존에 간 존에 대해 이야기하는 스터디입니다... 나이, 성별 제한 없습니다... 아무나 오세요... 환영합니다... 칼수락 합니다...",
+                "studyImg": "/img/sample-img-2.png",
+                "studyPersonNum": 4,
+                "studyStartDate": "2023-01-01",
+                "studyEndDate": "2023-12-31",
+                "modifyDate": "2023-02-23"
+            },
+            {
+                "studyBoardPk": 3,
+                "studyPk": 3,
+                "studyName": "우아한 스터디",
+                "adTitle": "우아한 스터디 모집합니다.",
+                "adContent": "우아한 스터디 모집중입니다! 우아한 스터디 모집중입니다! 우아한 스터디 모집중입니다! 우아한 스터디 모집중입니다! 우아한 스터디 모집중입니다! 우아한 스터디 모집중입니다!",
+                "studyImg": "/img/sample-img-3.jpg",
+                "studyPersonNum": 4,
+                "studyStartDate": "2023-01-01",
+                "studyEndDate": "2023-12-31",
+                "modifyDate": "2023-02-23"
+            }
+        ])
     );
-  }),
-  // MSW handler for specific study details based on studyBoardPk
+}),
+
 rest.get('/board/:studyBoardPk', (req, res, ctx) => {
   const { studyBoardPk } = req.params; // req.params를 통해 URL에서 studyBoardPk 값을 얻음
-  const boardList = [
-      {
-          "studyBoardPk": 1,
-          "studyPk": 1,
-          "adTitle": "빌런저장소입니다",
-          "adContent": "알고리즘 골드 이상까지 풀어 보실분! Java로 하시는 분 모집합니다! 활발한 분위기고 출석체크 엄격하게 합니다! 스터디 운영 여러개 해봤습니다! 다들 지원해주세요!",
-          "studyImg": "/img/sample-img-1.png",
-          "studyPersonNum": 4,
-          "studyStartDate": "2023-01-01",
-          "studyEndDate": "2023-12-31",
-          "modifyDate": "2023-02-23",
-      },
-      {
-          "studyBoardPk": 2,
-          "studyPk": 2,
-          "adTitle": "아마존에 간 건 아마...존?",
-          "adContent": "아마존에 간 존에 대해 이야기하는 스터디입니다... 나이, 성별 제한 없습니다... 아무나 오세요... 환영합니다... 칼수락 합니다...",
-          "studyImg": "/img/sample-img-2.png",
-          "studyPersonNum": 4,
-          "studyStartDate": "2023-01-01",
-          "studyEndDate": "2023-12-31",
-          "modifyDate": "2023-02-23",
-      },
-      {
-          "studyBoardPk": 3,
-          "studyPk": 3,
-          "adTitle": "우아한 스터디 모집합니다.",
-          "adContent": "우아한 스터디 모집중입니다! 우아한 스터디 모집중입니다! 우아한 스터디 모집중입니다! 우아한 스터디 모집중입니다! 우아한 스터디 모집중입니다! 우아한 스터디 모집중입니다!",
-          "studyImg": "/img/sample-img-3.jpg",
-          "studyPersonNum": 4,
-          "studyStartDate": "2023-01-01",
-          "studyEndDate": "2023-12-31",
-          "modifyDate": "2023-02-23",
-      }
-  ];
 
-  // 해당 studyBoardPk와 일치하는 객체 찾기
-  const study = boardList.find(study => study.studyBoardPk.toString() === studyBoardPk);
+  // 사전에 정의된 스터디 정보 객체
+  const boardData = {
+    "1": {
+      "studyBoardPk": 1,
+      "studyPk": 1,
+      "studyName": "빌런저장소",
+      "adTitle": "빌런저장소입니다",
+      "adContent": "알고리즘 골드 이상까지 풀어 보실분! Java로 하시는 분 모집합니다! 활발한 분위기고 출석체크 엄격하게 합니다! 스터디 운영 여러개 해봤습니다! 다들 지원해주세요!",
+      "studyImg": "/img/sample-img-1.png",
+      "studyPersonNum": 4,
+      "studyStartDate": "2023-01-01",
+      "studyEndDate": "2023-12-31",
+      "modifyDate": "2023-02-23"
+    },
+    "2": {
+      "studyBoardPk": 2,
+      "studyPk": 2,
+      "studyName": "아마zone",
+      "adTitle": "아마존에 간 건 아마...존?",
+      "adContent": "아마존에 간 존에 대해 이야기하는 스터디입니다... 나이, 성별 제한 없습니다... 아무나 오세요... 환영합니다... 칼수락 합니다...",
+      "studyImg": "/img/sample-img-2.png",
+      "studyPersonNum": 4,
+      "studyStartDate": "2023-01-01",
+      "studyEndDate": "2023-12-31",
+      "modifyDate": "2023-02-23"
+    },
+    "3": {
+      "studyBoardPk": 3,
+      "studyPk": 3,
+      "studyName": "우아한 스터디",
+      "adTitle": "우아한 스터디 모집합니다.",
+      "adContent": "우아한 스터디 모집중입니다! 우아한 스터디 모집중입니다! 우아한 스터디 모집중입니다! 우아한 스터디 모집중입니다! 우아한 스터디 모집중입니다! 우아한 스터디 모집중입니다!",
+      "studyImg": "/img/sample-img-3.jpg",
+      "studyPersonNum": 4,
+      "studyStartDate": "2023-01-01",
+      "studyEndDate": "2023-12-31",
+      "modifyDate": "2023-02-23"
+    }
+  };
+
+  const study = boardData[studyBoardPk];
 
   if (study) {
-      return res(
-          ctx.status(200),
-          ctx.json(study)
-      );
+    return res(ctx.status(200), ctx.json(study));
   } else {
-      return res(
-          ctx.status(404),
-          ctx.json({ error: "해당 스터디가 없습니다." })
-      );
+    return res(ctx.status(404), ctx.json({ error: "해당 스터디가 없습니다." }));
   }
-})
+}),
 
-  
-];
+]
