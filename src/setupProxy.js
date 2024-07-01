cadc15375f7e21f7344f71642a0d5605f25d6679
savedinstancestate <dayoung2335@naver.com>
2024-07-01 11:16:51 +0900

    [chore] 프록시 및 서버 경로 수정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     if (process.env.REACT_APP_USE_PROXY === 'true') {
   app.use(
     createProxyMiddleware({
-      target: 'http://ec2-43-203-206-205.ap-northeast-2.compute.amazonaws.com:8080',
+      target: 'http://ec2-43-203-206-205.ap-northeast-2.compute.amazonaws.com',
       changeOrigin: true,
     })
   );
