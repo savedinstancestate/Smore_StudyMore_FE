aa4d41c763ed8fc049bf82d53132aba5df58afc9
savedinstancestate <dayoung2335@naver.com>
2024-07-02 17:17:42 +0900

    [feat] 스터디 관리 탭 생성

---
+++ src/pages/StudyPage/Management/EditStudyInfo.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useParams, useNavigate } from 'react-router-dom';
+
+const EditStudyInfo = () => {
+
+};
+
+export default EditStudyInfo;
