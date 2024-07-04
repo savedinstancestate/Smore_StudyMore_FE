c38bcfe77e62b70719fa9a119564725d7a69214d
savedinstancestate <dayoung2335@naver.com>
2024-07-04 18:44:36 +0900

    [docs] 경로 변경

--- src/api/AxiosInstance.js
+++ src/api/AxiosInstance.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'localhost:8080',
-  // baseURL: `${process.env.REACT_APP_AUTH_URL}`,
+  // baseURL: '',
+  baseURL: `${process.env.REACT_APP_AUTH_URL}`,
   headers: {
     'Content-Type': 'application/json'
   }
