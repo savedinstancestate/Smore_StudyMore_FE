5df16507ba483add66d6fcef50737edc69bee542
savedinstancestate <dayoung2335@naver.com>
2024-07-07 20:29:33 +0900

    [feat] (방장기능) 스터디 멤버 관리 기능

--- src/pages/StudyPage/Management/EditMember.js
+++ src/pages/StudyPage/Management/EditMember.js
@@ -1,50 +1,100 @@
 import React, { useState, useEffect } from "react";
 import API from "../../../api/AxiosInstance";
+import Modal from '../../../components/Modal'; 
+import './Management.css';
 
 const EditMember = ({ studyPk }) => {
     const [members, setMembers] = useState([]);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedMemberPk, setSelectedMemberPk] = useState(null);
+    const [error, setError] = useState(null); 
+    const [successMessage, setSuccessMessage] = useState("");
 
     useEffect(() => {
         const fetchMembers = async () => {
           try {
             const response = await API.get(`/study/${studyPk}/dashboard/member`);
-            setMembers(response.data); 
+            setMembers(response.data);
           } catch (error) {
             console.error('멤버 정보를 불러오는데 실패했습니다.', error);
+            setError('멤버 정보를 불러오는데 실패했습니다.');
           }
         };
     
         fetchMembers();
-      }, [studyPk]);
+    }, [studyPk]);
 
-      return (
-        <div>
+    const handleExpelClick = (memberPk) => {
+      setSelectedMemberPk(memberPk);
+      setIsModalOpen(true);
+    };
+
+    const expelMember = async () => {
+      try {
+        const response = await API.delete(`/study/${studyPk}/management/expel`, {
+            data: { memberPk: selectedMemberPk }
+        });
+
+        if (response.status === 200) {
+          setMembers(members.filter(member => member.memberPk !== selectedMemberPk));
+          setSuccessMessage('멤버 퇴출이 성공적으로 처리되었습니다.');
+          setIsModalOpen(false);
+        } else {
+            console.error('상태 코드 에러:', error);
+            setIsModalOpen(false);
+        }
+      } catch (error) {
+        console.error('멤버 퇴출에 실패했습니다.', error);
+        setError('멤버 퇴출에 실패했습니다.');
+        setIsModalOpen(false);
+      }
+    };
+
+    const closeModal = () => {
+      setIsModalOpen(false);
+      setError(null); // 모달 닫을 때 에러 메시지 초기화
+      setSuccessMessage(""); // 성공 메시지도 초기화
+    };
+
+    return (
+        <div className="container">
             <div className="list-container">
-          <div className="title">멤버 관리</div>
-          <ul>
-            {members.map(member => (
-              <li key={member.memberPk} className="member-item">
-                <div
-                  className="member-img"
-                  alt={member.profileImg}
-                  style={{ backgroundImage: `url(${member.profileImg})` }}
-                ></div>
-                <div className="member-info">
-                  <p className="member-nickname">{member.nickName}</p>
+              <div className="title">멤버 관리 👫</div>
+              {error && <div className="alert alert-danger">{error}</div>}
+              {successMessage && <div className="alert alert-primary">{successMessage}</div>}
+              <ul>
+                {members.map(member => (
+                  <li key={member.memberPk} className="member-item">
+                    <div
+                      className="member-img"
+                      style={{ backgroundImage: `url(${member.profileImg})` }}
+                    ></div>
+                    <div className="member-info">
+                      <p className="member-nickname">{member.nickName}</p>
+                    </div>
+                    <button
+                        className="member-delete-btn"
+                        onClick={() => handleExpelClick(member.memberPk)}
+                      >
+                        퇴출
+                      </button>
+                  </li>
+                ))}
+              </ul>
+              
+              <Modal show={isModalOpen} handleClose={closeModal} title="멤버 퇴출">
+                <div>
+                  <h2>멤버 퇴출</h2>
+                  <p>정말로 멤버를 퇴출하시겠습니까?</p>
+                  <div className="modal-buttons">
+                    <button onClick={expelMember}>확인</button>
+                    <button onClick={closeModal}>취소</button>
+                  </div>
                 </div>
-                <button
-                    className="member-delete-btn"
-                  >
-                    삭제
-                  </button>
-                
-              </li>
-            ))}
-          </ul>
-          </div>
+              </Modal>
+            </div>
         </div>
-      );
-
+    );
 };
 
 export default EditMember;
