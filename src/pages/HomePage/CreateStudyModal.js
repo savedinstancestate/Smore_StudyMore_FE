921b8a02c084c3705d8ce5ea5df9e98513d2327c
savedinstancestate <dayoung2335@naver.com>
2024-07-09 14:40:23 +0900

    [style] 스터디 생성 버튼 이미지 경로 변경

--- src/pages/HomePage/CreateStudyModal.js
+++ src/pages/HomePage/CreateStudyModal.js
@@ -5,7 +5,7 @@ import { Modal, Form, Button } from 'react-bootstrap';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import moment from 'moment';
-import createImage from "./create.png";
+import createImage from "./createImage.png";
 
 const CreateStudyModal = () => {
     const [modalShow, setModalShow] = useState(false);
@@ -201,7 +201,7 @@ const CreateStudyModal = () => {
             border: 'none',
             borderRadius:'6px'}}
             >
-                <createImage src={createImage} style={{width: '22px', marginRight: '6px', marginTop: '-2px'}} />
+                <img src={createImage} style={{width: '22px', marginRight: '6px', marginTop: '-2px'}} />
                 스터디 생성하기
             </Button>
 
