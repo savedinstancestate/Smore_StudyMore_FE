babbf4151e89e86c449cf891652ac0119cdd0d1b
savedinstancestate <dayoung2335@naver.com>
2024-08-05 10:13:53 +0900

    [docs] 프록시 및 env 관련 변경

--- src/api/AxiosInstance.js
+++ src/api/AxiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const API = axios.create({
-  baseURL: 'http://ec2-43-202-82-233.ap-northeast-2.compute.amazonaws.com:8080',
+  baseURL: process.env.REACT_APP_AUTH_URL,
   headers: {
     'Content-Type': 'application/json'
   }
